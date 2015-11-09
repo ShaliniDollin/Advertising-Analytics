af3044de6d35ec810c749fc7e0199cd764a2225d
ShaliniDollin <1dreams4@gmail.com>
2015-11-08 22:55:15 -0800

    dashboeard header correction

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if ('development' === app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/login', routes.login);
-app.post('/dashboard', routes.dashboard);
+app.get('/dashboard', routes.dashboard);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
